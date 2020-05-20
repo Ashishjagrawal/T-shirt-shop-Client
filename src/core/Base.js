37ef4c0ccbb7167f2e1c3b959ad32bd8f796a45e
Ashish jagrawal <akjagrawal24@gmail.com>
2020-05-20 18:01:59 +0530

    Update Base.js

--- src/core/Base.js
+++ src/core/Base.js
@@ -22,12 +22,58 @@ const Base = ({
         <h4>If you got any questions, feel free to reach out!</h4>
         <button className="btn btn-primary btn-lg">Contact Us</button>
       </div>
-      <div className="container">
+      
+    </footer>
+    
+<footer class="container py-5">
+  <div class="row">
+    <div class="col-12 col-md">
+    <h5>About</h5>
+      <p class="d-block mb-3 text-muted">We have amazing T-Shirt Collections.
+       We will deliver it at your door as soon as possible.</p>
+    </div>
+    <div class="col-6 col-md">
+      <h5>Quick Links</h5>
+      <ul class="list-unstyled text-small">
+        <li><a class="text-muted" href="#">Sign In</a></li>
+        <li><a class="text-muted" href="#">Dashboard</a></li>
+        <li><a class="text-muted" href="#">Cart</a></li>
+      </ul>
+    </div>
+    <div class="col-6 col-md">
+      <h5>Categories</h5>
+      <ul class="list-unstyled text-small">
+        <li><a class="text-muted" href="#">Mens</a></li>
+        <li><a class="text-muted" href="#">Womens</a></li>
+        <li><a class="text-muted" href="#">Kids</a></li>
+        <li><a class="text-muted" href="#">Teens</a></li>
+      </ul>
+    </div>
+    <div class="col-6 col-md">
+      <h5>Collections</h5>
+      <ul class="list-unstyled text-small">
+        <li><a class="text-muted" href="#">Winter</a></li>
+        <li><a class="text-muted" href="#">Summer</a></li>
+        <li><a class="text-muted" href="#">Full Sleeves</a></li>
+        <li><a class="text-muted" href="#">Round Neck</a></li>
+      </ul>
+    </div>
+    <div class="col-6 col-md">
+      <h5>Contact Us:</h5>
+      <ul class="list-unstyled text-small">
+        <li><a class="text-muted" > 2/168, Sector 2, Jawahar Nagar, Jaipur </a></li>
+        <li><a class="text-muted" > Email: akjagrawal24@gmail.com </a></li>
+        <li><a class="text-muted" > Mobile No. : +917690892950 </a></li>
+        
+      </ul>
+    </div>
+  </div>
+  <div className="container">
         <span className="text-muted">
-        <span className="text-info font-weight-bold">Developed By:</span> <span className="font-weight-bold">Ashish Jagrawal</span> 
+            &copy; 2020 <span className="text-info font-weight-bold" >Developed By:</span> <span className="font-weight-bold">Ashish Jagrawal</span> 
         </span>
       </div>
-    </footer>
+</footer>
   </div>
 );
 
